b74feecd9ab4cb3e8b4328a741b1c355b9c69611
emincin.macos <emincin.life@gmail.com>
2025-09-15 16:05:52 +0800

    update c/string/main.c

--- c/string/main.c
+++ c/string/main.c
@@ -1,7 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+
 typedef struct {
   int len;
   char* buf;
 } String;
+
+void StringAppend(String* str, const char* s) {
+  if (str == NULL) return;
+  if (s == NULL) return;
+}
 
 int main(int argc, char** argv) {
   return 0;
