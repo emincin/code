87b1ee5b8056dbb4ac149d56136de57921d4850e
emincin.macos <emincin.life@gmail.com>
2025-09-16 07:47:20 +0800

    update c/print_any/main.c

--- c/print_any/main.c
+++ c/print_any/main.c
@@ -1,3 +1,25 @@
+#include <stdint.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define TYPE_I8 1
+#define TYPE_I16 2
+#define TYPE_I32 3
+#define TYPE_I64 4
+
+#define TYPE_U8 5
+#define TYPE_U16 6
+#define TYPE_U32 7
+#define TYPE_U64 8
+
 int main(int argc, char** argv) {
   return 0;
 }
