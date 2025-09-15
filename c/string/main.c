9bc7c7c06fd2fbee913829daa862d6c5d09af8e2
emincin.macos <emincin.life@gmail.com>
2025-09-15 16:24:40 +0800

    update c/string/main.c

--- c/string/main.c
+++ c/string/main.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct {
   int len;
@@ -9,6 +10,9 @@ typedef struct {
 void StringAppend(String* str, const char* s) {
   if (str == NULL) return;
   if (s == NULL) return;
+  int len = strlen(s);
+  if (len == 0) return;
+  int newlen = str->len + len;
 }
 
 int main(int argc, char** argv) {
