d28a029a532eff5f5d6a5cfe96fb004b37cc2374
emincin.macos <emincin.life@gmail.com>
2025-09-21 21:55:50 +0800

    update c/print_color/main.c

--- c/print_color/main.c
+++ c/print_color/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+void print_fg_idx(const char* s, int idx) {
+}
+
+void print_fg_rgb(const char* s, int r, int g, int b) {
+}
+
+void print_bg_idx(const char* s, int idx) {
+}
+
+void print_bg_rgb(const char* s, int r, int g, int b) {
+}
+
+void print_colors_idx(const char* s, int fg_idx, int bg_idx) {
+}
+
+void print_colors_rgb(const char* s, int fg_r, int fg_g, int fg_b, int bg_r, int bg_g, int bg_b) {
+}
+
 int main(int argc, char** argv) {
   return 0;
 }
