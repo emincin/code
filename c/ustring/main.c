26988e134e3d3202c23ab5b48c5c0c68525804c7
emincin.macos <emincin.life@gmail.com>
2025-09-24 18:25:17 +0800

    update c/ustring/main.c

--- c/ustring/main.c
+++ c/ustring/main.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
   size_t size;
@@ -11,6 +13,15 @@ typedef struct {
   size_t capacity;
   int* buffer;
 } String32;
+
+bool string_alloc(String* str, size_t capacity) {
+  if (str == NULL) return false;
+  str->size = 0;
+  str->capacity = 0;
+  str->buffer = NULL;
+  if (capacity == 0) return false;
+  return true;
+}
 
 int main(int argc, char** argv) {
   return 0;
