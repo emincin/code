6b41487ef3449d3c1cb977c01cb38f829c85351b
emincin.macos <emincin.life@gmail.com>
2025-09-24 13:07:26 +0800

    update c/ustring/main.c

--- c/ustring/main.c
+++ c/ustring/main.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
+typedef struct {
+  size_t size;
+  size_t capacity;
+  char* buffer;
+} String;
+
+typedef struct {
+  size_t size;
+  size_t capacity;
+  int* buffer;
+} String32;
+
 int main(int argc, char** argv) {
   return 0;
 }
