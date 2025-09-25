c0152e8f8b2a5994558301ee2883bd3b612740e6
emincin.macos <emincin.life@gmail.com>
2025-09-25 20:07:03 +0800

    update c/dstring/main.c

--- c/dstring/main.c
+++ c/dstring/main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+  size_t size;
+  size_t capacity;
+  char* buffer;
+} DString;
+
 int main(int argc, char** argv) {
   return 0;
 }
