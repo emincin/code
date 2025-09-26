baf04aa63f1d9787f33fdd67520594b2ccfacb34
emincin.macos <emincin.life@gmail.com>
2025-09-26 15:18:47 +0800

    update c/ds/ds.h

--- c/ds/ds.h
+++ c/ds/ds.h
@@ -1,6 +1,15 @@
 #ifndef DS_HEADER_FILE
 #define DS_HEADER_FILE
 #include <stddef.h>
+
+#define DS_CAPACITY 32
+
+typedef struct {
+  size_t size;
+  size_t capacity;
+  char* data;
+} ds;
+
 #endif // DS_HEADER_FILE
 
 #ifdef DS_IMPL
