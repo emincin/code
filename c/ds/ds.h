9d836c2ac1c9497e77e5820ffc788adef310d71a
emincin.macos <emincin.life@gmail.com>
2025-09-26 15:36:09 +0800

    update c/ds/ds.h

--- c/ds/ds.h
+++ c/ds/ds.h
@@ -1,8 +1,12 @@
 #ifndef DS_HEADER_FILE
 #define DS_HEADER_FILE
 #include <stddef.h>
+#include <stdlib.h>
 
 #define DS_CAPACITY 32
+
+#define make_ds(capacity) { 0, capacity, calloc(capacity + 1, sizeof(char)) }
+#define free_ds(ds) free(ds.data)
 
 typedef struct {
   size_t size;
