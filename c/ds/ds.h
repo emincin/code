cf7b14f33a4860df14b2d74f227e36f10542015a
emincin.macos <emincin.life@gmail.com>
2025-09-26 14:55:06 +0800

    remove string, ustring, dstring, add ds (dynamic string)

---
+++ c/ds/ds.h
@@ -0,0 +1,7 @@
+#ifndef DS_HEADER_FILE
+#define DS_HEADER_FILE
+#include <stddef.h>
+#endif // DS_HEADER_FILE
+
+#ifdef DS_IMPL
+#endif
---
+++ c/ds/main.c
@@ -0,0 +1,6 @@
+#define DS_IMPL
+#include "ds.h"
+
+int main(int argc, char** argv) {
+  return 0;
+}
