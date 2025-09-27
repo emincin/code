2b005daedd3df4838fa1e6fcff33f99ce6a97b31
emincin.macos <emincin.life@gmail.com>
2025-09-27 10:38:07 +0800

    add c/utf8/main.c

---
+++ c/utf8/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool in_range(int value, int min, int max) {
+  return min <= value && value <= max;
+}
+
+int main(int argc, char** argv) {
+  return 0;
+}
