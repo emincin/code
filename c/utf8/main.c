b94ca27ed8d5a215e66273658b500b8c17a0ca4e
emincin.macos <emincin.life@gmail.com>
2025-09-27 12:13:37 +0800

    update c/utf8/main.c

--- c/utf8/main.c
+++ c/utf8/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define ERR_UNKNOWN 0
+#define ERR_INVALID_CODE_POINT -1
+
 bool in_range(int value, int min, int max) {
   return min <= value && value <= max;
+}
+
+int code_point_to_utf8(int code_point, char* buf, int len) {
+  return ERR_UNKNOWN;
 }
 
 int main(int argc, char** argv) {
