9f1519220fe433b763daece0a0118ed0770eedf5
emincin.macos <emincin.life@gmail.com>
2025-09-27 18:21:36 +0800

    update c/utf8/main.c

--- c/utf8/main.c
+++ c/utf8/main.c
@@ -15,6 +15,20 @@ bool in_range(int value, int min, int max) {
   return min <= value && value <= max;
 }
 
+char leading_byte(int code_point, int rindex) {
+  code_point = code_point >> 6 * rindex;
+  int test_mask = 0x1F >> (rindex - 1);
+  int set_mask = 0xF0 << (3 - rindex);
+  return (char)(code_point & test_mask | set_mask);
+}
+
+char continuation_byte(int code_point, int rindex) {
+  code_point = code_point >> 6 * rindex;
+  int test_mask = 0x3F;
+  int set_mask = 0x80;
+  return (char)(code_point & test_mask | set_mask);
+}
+
 int code_point_to_utf8(int code_point, char* buf, int len) {
   if (!in_range(code_point, CODE_POINT_MIN, CODE_POINT_MAX)) {
     return ERR_INVALID_CODE_POINT;
@@ -22,10 +36,11 @@ int code_point_to_utf8(int code_point, char* buf, int len) {
   if (in_range(code_point, SURROGATE_MIN, SURROGATE_MAX)) {
     return ERR_INVALID_CODE_POINT;
   }
-  if (code_point <= 0x7F) {
-    if (len < 1) return ERR_BUF_SIZE_TOO_SMALL;
+  if (code_point < 128) {
+    int size = 1;
+    if (len < size) return ERR_BUF_SIZE_TOO_SMALL;
     buf[0] = (char)code_point;
-    return 1;
+    return size;
   }
   return ERR_UNKNOWN;
 }
