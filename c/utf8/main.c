a26b7f4d36690ebe9f58ea74ab3a93c283cf2300
emincin.macos <emincin.life@gmail.com>
2025-09-27 14:37:21 +0800

    update c/utf8/main.c

--- c/utf8/main.c
+++ c/utf8/main.c
@@ -9,6 +9,7 @@
 
 #define ERR_UNKNOWN 0
 #define ERR_INVALID_CODE_POINT -1
+#define ERR_BUF_SIZE_TOO_SMALL -2
 
 bool in_range(int value, int min, int max) {
   return min <= value && value <= max;
@@ -20,6 +21,11 @@ int code_point_to_utf8(int code_point, char* buf, int len) {
   }
   if (in_range(code_point, SURROGATE_MIN, SURROGATE_MAX)) {
     return ERR_INVALID_CODE_POINT;
+  }
+  if (code_point <= 0x7F) {
+    if (len < 1) return ERR_BUF_SIZE_TOO_SMALL;
+    buf[0] = (char)code_point;
+    return 1;
   }
   return ERR_UNKNOWN;
 }
