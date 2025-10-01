d47ea7dc3eaae5cc7988d4e660bd436643788c2c
emincin.macos <emincin.life@gmail.com>
2025-10-01 13:32:24 +0800

    update c/overload/main.c

--- c/overload/main.c
+++ c/overload/main.c
@@ -9,6 +9,8 @@
 #define COUNT(...) __VA_OPT__(COUNT_IMPL(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
 #define COUNT_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, n, ...) n
 
+#define NO_PARAM(...) (sizeof(#__VA_ARGS__) == 1)
+
 int main(int argc, char** argv) {
   return 0;
 }
