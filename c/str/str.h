b4a5b0a43e4834db1a2b6c93bab1b35f0e0eac4e
emincin.macos <emincin.life@gmail.com>
2025-10-06 11:28:53 +0800

    update c/str/str.h

--- c/str/str.h
+++ c/str/str.h
@@ -1,5 +1,13 @@
 #ifndef STR_H
 #define STR_H
+#include <stddef.h>
+
+typedef struct str_t {
+  size_t size;
+  size_t capacity;
+  char* data;
+} Str;
+
 #endif // STR_H
 #ifdef STR_C
 #endif // STR_C
