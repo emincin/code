1f7799e2003164a92e6a51e4f03f72f4152b316a
emincin.macos <emincin.life@gmail.com>
2025-10-07 11:03:20 +0800

    update c/str/str.h

--- c/str/str.h
+++ c/str/str.h
@@ -15,8 +15,10 @@ typedef struct str_t {
 Str* str_new_cstr_n(const char* buf, size_t len);
 Str* str_new_cstr(const char* buf);
 Str* str_new_capacity(size_t capacity);
-Str* str_new_();
+Str* str_new_(void);
 void str_delete(Str* str);
+size_t str_append_cstr_n(Str* str, const char* buf, size_t len);
+size_t str_append_cstr(Str* str, const char* buf);
 void str_clear(Str* str);
 void str_reset(Str* str);
 
@@ -73,7 +75,7 @@ clean_up:
   return NULL;
 }
 
-Str* str_new_() {
+Str* str_new_(void) {
   return str_new_capacity(STR_CAPACITY);
 }
 
@@ -83,6 +85,32 @@ void str_delete(Str* str) {
   }
   free(str->data);
   free(str);
+}
+
+size_t str_append_cstr_n(Str* str, const char* buf, size_t len) {
+  if (buf == NULL || len == 0) return 0;
+  size_t new_size = str->size + len;
+  if (new_size > str->capacity) {
+    size_t new_capacity = str->capacity;
+    while (new_capacity < new_size) {
+      new_capacity *= 2;
+    }
+    char* buffer = (char*)realloc(str->data, new_capacity + 1);
+    if (buffer == NULL) {
+      return 0;
+    }
+    memset(buffer + str->size, 0, new_capacity + 1 - str->size);
+    str->data = buffer;
+    str->capacity = new_capacity;
+  }
+  memcpy(str->data + str->size, buf, len);
+  str->size = new_size;
+  return len;
+}
+
+size_t str_append_cstr(Str* str, const char* buf) {
+  if (buf == NULL) return 0;
+  return str_append_cstr_n(str, buf, strlen(buf));
 }
 
 void str_clear(Str* str) {
