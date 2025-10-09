7c9af00d9edbfe5415be51a774df1ade6390978f
emincin.macos <emincin.life@gmail.com>
2025-10-09 23:23:14 +0800

    update c/str/main.c

--- c/str/main.c
+++ c/str/main.c
@@ -4,14 +4,24 @@
 void test_1(void) {
   Str* a = str_new_cstr("hi");
   str_append_cstr(a, " from str.h");
-  str_print(a);
+  str_print(a); // size:13 capacity:16 data:hi from str.h
   str_delete(a);
 }
 
 void test_2(void) {
   Str* a = str_new_char_n('x', 3);
   str_insert_cstr(a, 0, "123");
-  str_print(a);
+  str_print(a); // size:6 capacity:8 data:123xxx
+  str_delete(a);
+}
+
+void test_3(void) {
+  Str* a = str_new_cstr_n("123456", 3);
+  str_println(a); // size:3 capacity:4 data:123
+  str_resize(a, 2, 'x');
+  str_println(a); // size:2 capacity:4 data:12
+  str_resize(a, 6, 'z');
+  str_println(a); // size:6 capacity:8 data:12zzzz
   str_delete(a);
 }
 
@@ -21,8 +31,8 @@ void test_generic(void) {
   str_append(a, " is good!");
   Str* b = str_substr(a, 7);
   str_insert(b, 0, "auto");
-  str_println(a);
-  str_println(b);
+  str_println(a); // size:16 capacity:16 data:Generic is good!
+  str_println(b); // size:13 capacity:16 data:auto is good!
   str_delete(a);
   str_delete(b);
 }
