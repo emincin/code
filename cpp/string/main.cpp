355ff0e1a06f79e41a28e38cf50627441901ad9a
emincin.macos <emincin.life@gmail.com>
2025-10-12 23:35:32 +0800

    update cpp/string/main.cpp

--- cpp/string/main.cpp
+++ cpp/string/main.cpp
@@ -1,5 +1,11 @@
+#include <print> // C++23
 #include <string>
 #include <iostream>
+
+void test_1() {
+  std::string a;
+  std::println("capacity: {}", a.capacity());
+}
 
 int main(int argc, char** argv) {
   return 0;
