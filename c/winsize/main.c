33ac9f1433a188397c36e9117a4189f611bc8ed2
emincin.macos <emincin.life@gmail.com>
2025-10-13 18:56:35 +0800

    update c/winsize/main.c

--- c/winsize/main.c
+++ c/winsize/main.c
@@ -2,8 +2,21 @@
 #include <windows.h>
 #else
 #include <sys/ioctl.h>
-#endif
+#endif // _WIN32
 #include <stdio.h>
+
+typedef struct {
+  int width;
+  int height;
+} Size;
+
+Size term_get_window_size(void) {
+  Size size = { 0 };
+#if defined(_WIN32)
+#else
+#endif // _WIN32
+  return size;
+}
 
 int main(int argc, char** argv) {
   return 0;
