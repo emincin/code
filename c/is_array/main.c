5a3ae28bf71caa072c024dac793c261c2e1297de
emincin.macos <emincin.life@gmail.com>
2025-10-14 11:27:08 +0800

    update c/is_array/main.c

--- c/is_array/main.c
+++ c/is_array/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#define PTR_TEST(expr) _Generic((expr), \
+  char*: "char*", \
+  char[sizeof(expr)]: "char[]", \
+  default: "none" \
+)
+
 typedef struct {
   size_t size;
   char* data;
