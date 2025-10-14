2c32c1730640c77abbe8141237e5fba102390d1d
emincin.macos <emincin.life@gmail.com>
2025-10-14 12:43:35 +0800

    update c/is_array/main.c

--- c/is_array/main.c
+++ c/is_array/main.c
@@ -6,6 +6,11 @@
   default: "none" \
 )
 
+#define is_array(x) _Generic(&(x), \
+  typeof(*(x)) (*)[]: 1, \
+  default: 0 \
+)
+
 typedef struct {
   size_t size;
   char* data;
@@ -16,6 +21,18 @@ void ptr_test(void) {
   printf("buf: %s\n", PTR_TEST(buf));
   char* ptr = NULL;
   printf("ptr: %s\n", PTR_TEST(ptr));
+}
+
+char* get_rvalue(void) {
+  return NULL;
+}
+
+void is_array_test(void) {
+  char buf[32] = { 0 };
+  printf("buf is array: %d\n", is_array(buf));
+  char* ptr = NULL;
+  printf("ptr is array: %d\n", is_array(ptr));
+  //is_array(get_rvalue()); // error: cannot take the address of an rvalue
 }
 
 int main(int argc, char** argv) {
