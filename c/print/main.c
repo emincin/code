0471eb006bc647e20c98acad885ee5e27db064a2
emincin.macos <emincin.life@gmail.com>
2025-10-21 12:22:32 +0800

    update c/print/main.c

--- c/print/main.c
+++ c/print/main.c
@@ -5,6 +5,23 @@
 #define CONCAT(a, b) CONCAT_IMPL(a, b)
 #define CONCAT_IMPL(a, b) a##b
 
+#define SELECT( \
+  _1, _2, _3, _4, \
+  _5, _6, _7, _8, \
+  _9, _10, _11, _12, \
+  _13, _14, _15, _16, \
+  name, ...) name
+
+#define COUNT_ARGS(...) __VA_OPT__( \
+  SELECT(__VA_ARGS__, \
+    16, 15, 14, 13, \
+    12, 11, 10, 9, \
+    8, 7, 6, 5, \
+    4, 3, 2, 1 \
+  ))
+
+#define ARGS_COUNT(...) (0 __VA_OPT__(+ COUNT_ARGS(__VA_ARGS__)))
+
 #define TYPE_NONE           0
 #define TYPE_BOOL           1
 #define TYPE_CHAR           2
