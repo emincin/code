b014a70194e0db56d732b32b877098c963b4984e
emincin.macos <emincin.life@gmail.com>
2025-11-06 22:33:12 +0800

    update c/is_same/main.c

--- c/is_same/main.c
+++ c/is_same/main.c
@@ -6,7 +6,18 @@ typedef struct {
   int y;
 } Player;
 
+void convert_test(void) {
+  //Player a = (Player)0; // compile-time error: arithmetic or pointer type is required
+  Player* b = (Player*)0; // OK
+}
+
+void is_same_test(void) {
+}
+
 void test(void) {
+#ifdef IS_SAME_TEST
+  is_same_test();
+#endif
 }
 
 int main(int argc, char** argv) {
