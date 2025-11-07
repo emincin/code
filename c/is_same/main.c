a0be96a6b8ed87664a051cec9a2877216a957597
emincin.macos <emincin.life@gmail.com>
2025-11-07 14:01:55 +0800

    update c/is_same/main.c

--- c/is_same/main.c
+++ c/is_same/main.c
@@ -7,11 +7,15 @@
 #define print_is_same(T1, T2) \
   printf("(%s) = (%s) : %d\n", #T1, #T2, is_same(T1, T2))
 
+#define print_is(a, b) printf("%s is %s\n", #a, #b)
+
 typedef struct {
   int id;
   int x;
   int y;
 } Player;
+
+#define make_player(id, x, y) ((Player){ id, x, y })
 
 void convert_test(void) {
   //Player a = (Player)0; // compile-time error: arithmetic or pointer type is required
@@ -39,6 +43,19 @@ void test_3(void) {
   print_is_same(const int *, int const *);
 }
 
+void test_4(void) {
+  auto a = 0;
+  if (is_same(typeof(a), int)) {
+    print_is(a, int);
+  } else if (is_same(typeof(a), double)) {
+    print_is(a, double);
+  } else if (is_same(typeof(a), char*)) {
+    print_is(a, char*);
+  } else if (is_same(typeof(a), Player)) {
+    print_is(a, Player);
+  }
+}
+
 void test(void) {
 #ifdef IS_SAME_TEST
   is_same_test();
@@ -48,6 +65,9 @@ void test(void) {
 #endif
 #ifdef TEST_3
   test_3();
+#endif
+#ifdef TEST_4
+  test_4();
 #endif
 }
 
