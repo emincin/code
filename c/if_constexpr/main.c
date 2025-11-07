499fcf411ade1f1eec778088b47f1baa9962c138
emincin.macos <emincin.life@gmail.com>
2025-11-08 02:26:23 +0800

    update c/if_constexpr/main.c

--- c/if_constexpr/main.c
+++ c/if_constexpr/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#define comptime_bool(expr) _Generic(&(char[1 + !!(expr)]){ 0 }, \
+  char (*)[2]: 1, \
+  char (*)[1]: 0)
+
+#define if_constexpr(expr) if (comptime_bool(expr))
+
+#define elif_constexpr(expr) else if (comptime_bool(expr))
+
 int main(int argc, char** argv) {
   return 0;
 }
