1699e783ee7b6ec3bf2e5c79badf34d1f120c543
emincin.macos <emincin.life@gmail.com>
2025-11-12 00:37:20 +0800

    update c/traits/main.c

--- c/traits/main.c
+++ c/traits/main.c
@@ -5,6 +5,18 @@
   const char*: 1, \
   default: 0)
 
+#define is_signed_type(T) ((T)(-1) < 0)
+
+#define is_unsigned_type(T) ((T)(-1) > 0)
+
+#define is_signed(expr) _Generic((expr), \
+  char: is_signed_type(char), \
+  default: 0)
+
+#define is_unsigned(expr) _Generic((expr), \
+  char: is_unsigned_type(char), \
+  default: 0)
+
 void test(void) {
 }
 
