5a12aadac634d47f476dec54fa8f2ec659a1bb77
emincin.macos <emincin.life@gmail.com>
2025-11-11 23:21:08 +0800

    update c/traits/main.c

--- c/traits/main.c
+++ c/traits/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#define is_c_str(expr) _Generic((expr), \
+  char*: 1, \
+  const char*: 1, \
+  default: 0)
+
+void test(void) {
+}
+
 int main(int argc, char** argv) {
+  test();
   return 0;
 }
