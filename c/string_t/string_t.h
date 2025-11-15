f2dbbbf8403e7056a626ad102c2f45dfa6f80eeb
emincin.macos <emincin.life@gmail.com>
2025-11-16 01:39:01 +0800

    update c/string_t/string_t.h

--- c/string_t/string_t.h
+++ c/string_t/string_t.h
@@ -1,5 +1,14 @@
 #ifndef __STRING_T__
 #define __STRING_T__
+
+#include <stddef.h>
+
+typedef struct string_t {
+  size_t capacity;
+  size_t size;
+  char* data;
+} String;
+
 #endif//__STRING_T__
 
 #if defined(STRING_T_IMPL)
