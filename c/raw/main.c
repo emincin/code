d0f6df25789170230ec45ee14a433ed4e39b04cd
emincin.macos <emincin.life@gmail.com>
2025-11-28 00:15:30 +0800

    update c/raw/main.c

--- c/raw/main.c
+++ c/raw/main.c
@@ -5,11 +5,11 @@
 int main(int argc, char** argv) {
   printf("Size of termios: %zu bytes\n", sizeof(struct termios));
   printf("Align of termios: %zu bytes\n", _Alignof(struct termios));
-  printf("Offset of c_iflag: %zu\n", offsetof(struct termios, c_iflag));
-  printf("Offset of c_oflag: %zu\n", offsetof(struct termios, c_oflag));
-  printf("Offset of c_cflag: %zu\n", offsetof(struct termios, c_cflag));
-  printf("Offset of c_lflag: %zu\n", offsetof(struct termios, c_lflag));
-  printf("Offset of c_cc: %zu\n", offsetof(struct termios, c_cc));
+  printf("Offset of c_iflag:  %zu\n", offsetof(struct termios, c_iflag));
+  printf("Offset of c_oflag:  %zu\n", offsetof(struct termios, c_oflag));
+  printf("Offset of c_cflag:  %zu\n", offsetof(struct termios, c_cflag));
+  printf("Offset of c_lflag:  %zu\n", offsetof(struct termios, c_lflag));
+  printf("Offset of c_cc:     %zu\n", offsetof(struct termios, c_cc));
   printf("Offset of c_ispeed: %zu\n", offsetof(struct termios, c_ispeed));
   printf("Offset of c_ospeed: %zu\n", offsetof(struct termios, c_ospeed));
   return 0;
