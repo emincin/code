efccc0b6b2d5ee76514e7f6774fe5abf26b4f355
emincin.macos <emincin.life@gmail.com>
2025-12-02 02:48:55 +0800

    Update main.c

--- c/raw/main.c
+++ c/raw/main.c
@@ -3,8 +3,16 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+  struct termios term = { 0 };
   printf("Size of termios: %zu bytes\n", sizeof(struct termios));
   printf("Align of termios: %zu bytes\n", _Alignof(struct termios));
+  printf("Size of c_iflag:  %zu\n", sizeof(term.c_iflag));
+  printf("Size of c_oflag:  %zu\n", sizeof(term.c_oflag));
+  printf("Size of c_cflag:  %zu\n", sizeof(term.c_cflag));
+  printf("Size of c_lflag:  %zu\n", sizeof(term.c_lflag));
+  printf("Size of c_cc:     %zu\n", sizeof(term.c_cc));
+  printf("Size of c_ispeed: %zu\n", sizeof(term.c_ispeed));
+  printf("Size of c_ospeed: %zu\n", sizeof(term.c_ospeed));
   printf("Offset of c_iflag:  %zu\n", offsetof(struct termios, c_iflag));
   printf("Offset of c_oflag:  %zu\n", offsetof(struct termios, c_oflag));
   printf("Offset of c_cflag:  %zu\n", offsetof(struct termios, c_cflag));
