e36e22f5c3f564d17c8ca25ca82bbd5499641f63
emincin.macos <emincin.life@gmail.com>
2025-12-02 18:39:21 +0800

    update c/tuple/main.c

--- c/tuple/main.c
+++ c/tuple/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#define let auto
+
+struct { int x; int y; } make_xy(int x, int y) {
+  return (typeof(make_xy(0, 0))){ x, y };
+}
+
 int main(int argc, char** argv) {
+  let pos = make_xy(2, 1);
+  printf("[x: %d, y: %d]\n", pos.x, pos.y);
   return 0;
 }
