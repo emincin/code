73db80419fe41fb370a771e030450267df469f91
emincin.macos <emincin.life@gmail.com>
2025-12-06 20:40:40 +0800

    update c/tuple/main.c

--- c/tuple/main.c
+++ c/tuple/main.c
@@ -6,8 +6,18 @@ struct { int x; int y; } make_xy(int x, int y) {
   return (typeof(make_xy(0, 0))){ x, y };
 }
 
-int main(int argc, char** argv) {
+void test_1(void) {
   let pos = make_xy(2, 1);
   printf("[x: %d, y: %d]\n", pos.x, pos.y);
+}
+
+void test(void) {
+#ifdef TEST_1
+  test_1();
+#endif
+}
+
+int main(int argc, char** argv) {
+  test();
   return 0;
 }
